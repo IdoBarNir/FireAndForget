0c7dcbfd15fedbd0c08f11a4fde71c3a3ed00881
Ido Bar-Nir <barnir@sheeping.com>
2022-09-11 12:45:50 +0300

    new init 3

--- fireandforget/src/components/TaskInput/TaskInput.js
+++ fireandforget/src/components/TaskInput/TaskInput.js
@@ -1,22 +1,40 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import TextField from "@mui/material/TextField";
+import Input from "@mui/material/Input";
+
+import Button from "@mui/material/Button";
+import SendIcon from "@mui/icons-material/Send";
+import InputAdornment from "@mui/material/InputAdornment";
+
+const handleChange = () => {};
 
 const TaskInput = () => {
+  const [title, setTitle] = useState("");
+
   return (
-    <div className="Input">
-      <TextField
-        id="outlined-multiline-flexible"
-        label="New Task"
-        helperText="Enter new task"
-        multiline
-        fullWidth
-        margin="normal"
-        color="success"
-        maxRows={4}
-        value={"placeholder value"}
-      />
-    </div>
+    <TextField
+      id="outlined-multiline-flexible"
+      label="New Task"
+      helperText="Enter new task"
+      multiline
+      fullWidth
+      margin="normal"
+      color="success"
+      maxRows={4}
+      value={title}
+      required
+      onChange={handleChange}
+      InputProps={{
+        endAdornment: (
+          <InputAdornment position="end">
+            <Button variant="contained" endIcon={<SendIcon />}>
+              Fire
+            </Button>
+          </InputAdornment>
+        ),
+      }}
+    />
   );
 };
 
